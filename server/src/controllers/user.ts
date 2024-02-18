f76c707f7cb9c8ed600833a3b68917213f0516ac
amitkr7 <amit840901@gmail.com>
2024-02-18 19:52:13 +0530

    Mailtrap Setup

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,7 +1,9 @@
 import { RequestHandler } from 'express';
+import nodemailer from 'nodemailer';
 
 import { CreateUser } from '../@types/user';
 import User from '../models/user';
+import { MAILTRAP_PASSWORD, MAILTRAP_USER } from '../utils/variables';
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -10,5 +12,21 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
   // user.save()
 
   const user = await User.create({ email, password, name });
+
+  const transport = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASSWORD,
+    },
+  });
+
+  transport.sendMail({
+    to: user.email,
+    from: 'auth@podify.com',
+    html: '<h1>12345</h1>',
+  });
+
   res.status(201).json({ user });
 };
