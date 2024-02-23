8edc30b073d8bf4ccf1e6c766151120da6c1115c
amitkr7 <amit840901@gmail.com>
2024-02-23 22:24:34 +0530

    Separating email send functionalaity

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,13 +1,9 @@
 import { RequestHandler } from 'express';
-import nodemailer from 'nodemailer';
-import path from 'path';
 
 import { CreateUser } from '../@types/user';
 import User from '../models/user';
-import EmailVerificationToken from '../models/emailVerificationToken';
 import { generateToken } from '../utils/helper';
-import { MAILTRAP_PASSWORD, MAILTRAP_USER } from '../utils/variables';
-import { generateTemplate } from '../mail/template';
+import { sendVerificationMail } from '../utils/mail';
 
 export const create: RequestHandler = async (req: CreateUser, res) => {
   const { email, password, name } = req.body;
@@ -17,48 +13,8 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
 
   const user = await User.create({ email, password, name });
 
-  const transport = nodemailer.createTransport({
-    host: 'sandbox.smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: MAILTRAP_USER,
-      pass: MAILTRAP_PASSWORD,
-    },
-  });
-
   const token = generateToken();
-  await EmailVerificationToken.create({
-    owner: user._id,
-    token,
-  });
+  sendVerificationMail(token, { name, email, userId: user._id.toString() });
 
-  const welcomeMessage = `Hi ${name}, Welcome to Podify! There are so much thing that you can do if email is verified. Use the given OTP to verify your email`;
-
-  transport.sendMail({
-    to: user.email,
-    from: 'auth@podify.com',
-    subject: 'Welcome to Podify',
-    html: generateTemplate({
-      title: 'Welcome to Podify',
-      message: welcomeMessage,
-      logo: 'cid:logo',
-      banner: 'cid:welcome',
-      link: '#',
-      btnTitle: token,
-    }),
-    attachments: [
-      {
-        filename: 'logo.png',
-        path: path.join(__dirname, '../mail/logo.png'),
-        cid: 'logo',
-      },
-      {
-        filename: 'welcome.png',
-        path: path.join(__dirname, '../mail/welcome.png'),
-        cid: 'welcome',
-      },
-    ],
-  });
-
-  res.status(201).json({ user });
+  res.status(201).json({ user: { id: user._id, name, email } });
 };
---
+++ server/src/utils/mail.ts
@@ -0,0 +1,67 @@
+import nodemailer from 'nodemailer';
+import path from 'path';
+
+import EmailVerificationToken from '../models/emailVerificationToken';
+import {
+  MAILTRAP_PASSWORD,
+  MAILTRAP_USER,
+  VERIFICATION_EMAIL,
+} from '../utils/variables';
+import { generateTemplate } from '../mail/template';
+
+const generateMailTransporter = () => {
+  const transport = nodemailer.createTransport({
+    host: 'sandbox.smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: MAILTRAP_USER,
+      pass: MAILTRAP_PASSWORD,
+    },
+  });
+  return transport;
+};
+
+interface Profile {
+  name: string;
+  email: string;
+  userId: string;
+}
+
+export const sendVerificationMail = async (token: string, profile: Profile) => {
+  const transport = generateMailTransporter();
+
+  const { name, email, userId } = profile;
+
+  await EmailVerificationToken.create({
+    owner: userId,
+    token,
+  });
+
+  const welcomeMessage = `Hi ${name}, Welcome to Podify! There are so much thing that you can do if email is verified. Use the given OTP to verify your email`;
+
+  transport.sendMail({
+    to: email,
+    from: VERIFICATION_EMAIL,
+    subject: 'Welcome to Podify',
+    html: generateTemplate({
+      title: 'Welcome to Podify',
+      message: welcomeMessage,
+      logo: 'cid:logo',
+      banner: 'cid:welcome',
+      link: '#',
+      btnTitle: token,
+    }),
+    attachments: [
+      {
+        filename: 'logo.png',
+        path: path.join(__dirname, '../mail/logo.png'),
+        cid: 'logo',
+      },
+      {
+        filename: 'welcome.png',
+        path: path.join(__dirname, '../mail/welcome.png'),
+        cid: 'welcome',
+      },
+    ],
+  });
+};
