736fb9e1b55a72e65a799308237208787a797283
amitkr7 <amit840901@gmail.com>
2024-02-24 22:29:33 +0530

    Verifying Token

--- server/src/controllers/user.ts
+++ server/src/controllers/user.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 
-import { CreateUser } from '../@types/user';
+import { CreateUser, VerifyEmailRequest } from '../@types/user';
+import EmailVerificationToken from '../models/emailVerificationToken';
 import User from '../models/user';
 import { generateToken } from '../utils/helper';
 import { sendVerificationMail } from '../utils/mail';
@@ -17,4 +18,26 @@ export const create: RequestHandler = async (req: CreateUser, res) => {
   sendVerificationMail(token, { name, email, userId: user._id.toString() });
 
   res.status(201).json({ user: { id: user._id, name, email } });
+};
+
+export const verifyEmail: RequestHandler = async (
+  req: VerifyEmailRequest,
+  res
+) => {
+  const { userId, token } = req.body;
+
+  const verificationToken = await EmailVerificationToken.findOne({
+    owner: userId,
+  });
+
+  if (!verificationToken) return res.status(403).json({ error: 'Invalid OTP' });
+
+  const matched = await verificationToken?.compareToken(token);
+
+  if (!matched) return res.status(403).json({ error: 'Invalid OTP' });
+
+  await User.findByIdAndUpdate(userId, { verified: true });
+  await EmailVerificationToken.findByIdAndDelete(verificationToken._id);
+
+  res.json({ message: 'Email Verified' });
 };
