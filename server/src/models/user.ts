77dde1633ba3e68bddc33ac0cc6178ad52f58181
amitkr7 <amit840901@gmail.com>
2024-02-24 20:47:26 +0530

    Hashing Password

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,3 +1,4 @@
+import { compare, hash } from 'bcrypt';
 import { Model, model, ObjectId, Schema } from 'mongoose';
 
 interface UserDocument {
@@ -15,7 +16,11 @@ interface UserDocument {
   following: ObjectId[];
 }
 
-const userSchema = new Schema<UserDocument>(
+interface Methods {
+  comparePassword(password: string): Promise<boolean>;
+}
+
+const userSchema = new Schema<UserDocument, {}, Methods>(
   {
     name: {
       type: String,
@@ -66,4 +71,16 @@ const userSchema = new Schema<UserDocument>(
   }
 );
 
-export default model('User', userSchema) as Model<UserDocument>;
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await hash(this.password, 10);
+  }
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const result = await compare(password, this.password);
+  return result;
+};
+
+export default model('User', userSchema) as Model<UserDocument, {}, Methods>;
