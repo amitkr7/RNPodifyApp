dac7e4e4fb3372fdcd7519f8684aaba1458e7d73
amitkr7 <amit840901@gmail.com>
2024-02-26 19:35:40 +0530

    Verifying Password reset token

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -3,20 +3,26 @@ import { Router } from 'express';
 import {
   create,
   generateForgotPasswordLink,
-  verifyEmail,
   sendReVerificationToken,
+  verifyEmail,
+  verifyResetPasswordToken,
 } from '../controllers/user';
 import { validate } from '../middleware/validator';
 import {
   CreateUserSchema,
-  EmailVerificationBody,
+  TokenAndIdValidation,
 } from '../utils/schemaValidator';
 
 const router = Router();
 
 router.post('/create', validate(CreateUserSchema), create);
-router.post('/verify-email', validate(EmailVerificationBody), verifyEmail);
+router.post('/verify-email', validate(TokenAndIdValidation), verifyEmail);
 router.post('/resend-verify-email', sendReVerificationToken);
 router.post('/forgot-password', generateForgotPasswordLink);
+router.post(
+  '/verify-reset-pass-token',
+  validate(TokenAndIdValidation),
+  verifyResetPasswordToken
+);
 
 export default router;
