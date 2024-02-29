51f24a2225bcd814665dae583c97b6fc09a6d3fe
amitkr7 <amit840901@gmail.com>
2024-02-29 21:37:46 +0530

    Updating password from UI

--- server/src/public/script.js
+++ server/src/public/script.js
@@ -17,6 +17,7 @@ loader.style.display = "block"
 container.style.display = "none"
 
 let token, userId
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#\$%^&\*])[a-zA-Z\d!@#\$%^&\*]+$/
 
 window.addEventListener('DOMContentLoaded', async () => {
     const params = new Proxy(new URLSearchParams(window.location.search), {
@@ -47,3 +48,54 @@ window.addEventListener('DOMContentLoaded', async () => {
     container.style.display = "block"
 })
 
+const displayError = (errorMsg) => {
+    success.style.display = "none"
+    error.innerText = errorMsg
+    error.style.display = "block"
+}
+const displaySuccess = (successMsg) => {
+    error.style.display = "none"
+    success.innerText = successMsg
+    success.style.display = "block"
+}
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!password.value.trim()) {
+        return displayError("Password is Missing")
+    }
+
+    if (!passwordRegex.test(password.value)) {
+        return displayError("Password is too Simple, Use alphanumeric with special character")
+    }
+
+    if (password.value !== confirmPassword.value) {
+        return displayError("Password and Confirm Password do not match!")
+    }
+
+    button.disabled = true;
+    button.innerText = "Please Wait"
+    const res = await fetch("/auth/update-password", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8"
+        },
+        body: JSON.stringify({
+            token, userId, password: password.value
+        })
+    })
+
+    button.disabled = false;
+    button.innerText = "Reset Password"
+
+    if (!res.ok) {
+        const { error } = await res.json()
+        return displayError(error)
+    }
+
+    displaySuccess("Your Password is reset Successfully!")
+    password.value = ""
+    confirmPassword.value = ""
+}
+
+form.addEventListener('submit', handleSubmit)
