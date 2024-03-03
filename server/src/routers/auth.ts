c2f2d29e01496a77bf587f4999938f9a9ae769c5
amitkr7 <amit840901@gmail.com>
2024-03-03 22:14:49 +0530

    FileParser Middleware

--- server/src/routers/auth.ts
+++ server/src/routers/auth.ts
@@ -10,6 +10,7 @@ import {
   verifyEmail,
 } from '../controllers/user';
 import { authenticateUser, verifyResetPasswordToken } from '../middleware/auth';
+import fileParser, { RequestWithFiles } from '../middleware/fileParser';
 import { validate } from '../middleware/validator';
 import {
   CreateUserSchema,
@@ -41,32 +42,9 @@ router.get('/is-auth', authenticateUser, (req, res) => {
   res.json({ profile: req.user });
 });
 
-import formidable from 'formidable';
-import path from 'path';
-import fs from 'fs';
-
-router.post('/update-profile', async (req, res) => {
-  if (!req.headers['content-type']?.startsWith('multipart/form-data;'))
-    return res.status(422).json({ error: 'Only accepts form-data' });
-
-  const dir = path.join(__dirname, '../public/profiles');
-
-  try {
-    await fs.readdirSync(dir);
-  } catch (error) {
-    await fs.mkdirSync(dir);
-  }
-
-  const form = formidable({
-    uploadDir: dir,
-    filename(name, ext, part, form) {
-      return Date.now() + '_' + part.originalFilename;
-    },
-  });
-
-  form.parse(req, (err, fields, files) => {
-    res.json({ uploaded: true });
-  });
+router.post('/update-profile', fileParser, (req: RequestWithFiles, res) => {
+  console.log(req.files);
+  res.json({ uploaded: true });
 });
 
 export default router;
