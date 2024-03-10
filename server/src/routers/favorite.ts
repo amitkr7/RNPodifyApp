ed1cd5e6affbc0c3d6e23c86729801e0d0c64957
amitkr7 <amit840901@gmail.com>
2024-03-10 23:05:37 +0530

    Get isFav

--- server/src/routers/favorite.ts
+++ server/src/routers/favorite.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { getFavorites, toggleFavorite } from '../controllers/favorite';
+import {
+  getFavorites,
+  getIsFavorite,
+  toggleFavorite,
+} from '../controllers/favorite';
 import { authenticateUser, isVerified } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/', authenticateUser, isVerified, toggleFavorite);
 router.get('/', authenticateUser, getFavorites);
+router.get('/is-fav', authenticateUser, getIsFavorite);
 
 export default router;
