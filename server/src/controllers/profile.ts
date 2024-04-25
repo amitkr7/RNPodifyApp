0e52c8fcd9cd04c77193f8d5a307505445e1540b
amitkr7 <amit840901@gmail.com>
2024-04-25 23:45:52 +0530

    Get All public uploads

--- server/src/controllers/profile.ts
+++ server/src/controllers/profile.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from 'express';
-import { isValidObjectId } from 'mongoose';
+import { isValidObjectId, ObjectId } from 'mongoose';
 import { paginationQuery } from '../@types/misc';
-import Audio from '../models/audio';
+import Audio, { AudioDocument } from '../models/audio';
 import User from '../models/user';
 
 export const updateFollower: RequestHandler = async (req, res) => {
@@ -72,6 +72,33 @@ export const getUploads: RequestHandler = async (req, res) => {
       poster: item.poster?.url,
       date: item.createdAt,
       owner: { name: req.user.name, id: req.user.id },
+    };
+  });
+
+  res.json({ audios });
+};
+export const getPublicUploads: RequestHandler = async (req, res) => {
+  const { page = '0', limit = '20' } = req.query as paginationQuery;
+  const { profileId } = req.params;
+
+  if (!isValidObjectId(profileId))
+    return res.status(422).json({ erorr: 'Invalid profile Id' });
+
+  const data = await Audio.find({ owner: profileId })
+    .skip(parseInt(limit) * parseInt(page))
+    .limit(parseInt(limit))
+    .sort('-createdAt')
+    .populate<AudioDocument<{ name: string; _id: ObjectId }>>('owner');
+
+  const audios = data.map((item) => {
+    return {
+      id: item._id,
+      title: item.title,
+      about: item.about,
+      file: item.file.url,
+      poster: item.poster?.url,
+      date: item.createdAt,
+      owner: { name: item.owner.name, id: item.owner._id },
     };
   });
 
--- server/src/routers/profile.ts
+++ server/src/routers/profile.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { getUploads, updateFollower } from '../controllers/profile';
+import {
+  getPublicUploads,
+  getUploads,
+  updateFollower,
+} from '../controllers/profile';
 import { authenticateUser } from '../middleware/auth';
 
 const router = Router();
 
 router.post('/update-profile/:profileId', authenticateUser, updateFollower);
 router.get('/uploads', authenticateUser, getUploads);
+router.get('/uploads/:profileId', getPublicUploads);
 
 export default router;
