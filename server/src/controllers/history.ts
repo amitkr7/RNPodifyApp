6c1f3bf2ba7db4eab54865a689490e406ed389b3
amitkr7 <amit840901@gmail.com>
2024-04-27 21:42:06 +0530

    Update History

--- server/src/controllers/history.ts
+++ server/src/controllers/history.ts
@@ -1,3 +1,71 @@
 import { RequestHandler } from 'express';
+import History, { historyType } from '../models/history';
 
-export const updateHistory: RequestHandler = (req, res) => {};
+export const updateHistory: RequestHandler = async (req, res) => {
+  const oldHistory = await History.findOne({ owner: req.user.id });
+
+  const { audio, progress, date } = req.body;
+
+  const history: historyType = { audio, progress, date };
+
+  if (!oldHistory) {
+    await History.create({
+      owner: req.user.id,
+      last: history,
+      all: [history],
+    });
+    return res.json({ success: true });
+  }
+
+  const today = new Date();
+  const startofDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate()
+  );
+  const endOfDay = new Date(
+    today.getFullYear(),
+    today.getMonth(),
+    today.getDate() + 1
+  );
+
+  const histories = await History.aggregate([
+    { $match: { owner: req.user.id } },
+    { $unwind: '$all' },
+    {
+      $match: {
+        'all.date': {
+          $gte: startofDay,
+          $lt: endOfDay,
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        audio: '$all.audio',
+      },
+    },
+  ]);
+
+  const sameDayHistory = histories.find((item) => {
+    if (item.audio.toString() === audio) return item;
+  });
+
+  if (sameDayHistory) {
+    await History.findOneAndUpdate(
+      {
+        owner: req.user.id,
+        'all.audio': audio,
+      },
+      { $set: { 'all.$.progress': progress, 'all.$.date': date } }
+    );
+  } else {
+    await History.findByIdAndUpdate(oldHistory._id, {
+      $push: { all: { $each: [history], $position: 0 } },
+      $set: { last: history },
+    });
+  }
+
+  res.json({ success: true });
+};
