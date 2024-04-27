e0cfc3d3be4f474e33fe6b8a0b643f006e6eac52
amitkr7 <amit840901@gmail.com>
2024-04-27 16:47:26 +0530

    History Schema Validator

--- server/src/routers/history.ts
+++ server/src/routers/history.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 import { updateHistory } from '../controllers/history';
 import { authenticateUser } from '../middleware/auth';
+import { validate } from '../middleware/validator';
+import { UpdateHistoryValidationSchema } from '../utils/schemaValidator';
 
 const router = Router();
 
-router.post('/', authenticateUser, updateHistory);
+router.post(
+  '/',
+  authenticateUser,
+  validate(UpdateHistoryValidationSchema),
+  updateHistory
+);
 
 export default router;
